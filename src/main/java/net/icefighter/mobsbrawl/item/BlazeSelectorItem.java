7c518aa87309a0c7fbbbe88470492af9fee797df
olliecay <122303892+olliecay@users.noreply.github.com>
2024-06-20 19:26:10 +0100

    Added sound effect

--- src/main/java/net/icefighter/mobsbrawl/item/BlazeSelectorItem.java
+++ src/main/java/net/icefighter/mobsbrawl/item/BlazeSelectorItem.java
@@ -1,7 +1,15 @@
 
 package net.icefighter.mobsbrawl.item;
 
-import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionHand;
+
+import net.icefighter.mobsbrawl.procedures.BlazeSelectorRightclickedProcedure;
 
 public class BlazeSelectorItem extends Item {
 	public BlazeSelectorItem() {
@@ -11,7 +19,7 @@ public class BlazeSelectorItem extends Item {
 	@Override
 	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
 		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
-		BlazeSelectorRightclickedProcedure.execute(entity, ar.getObject());
+		BlazeSelectorRightclickedProcedure.execute(world, entity.getX(), entity.getY(), entity.getZ(), entity, ar.getObject());
 		return ar;
 	}
 }
--- src/main/java/net/icefighter/mobsbrawl/procedures/FireboltRightclickedProcedure.java
+++ src/main/java/net/icefighter/mobsbrawl/procedures/FireboltRightclickedProcedure.java
@@ -1,5 +1,7 @@
 package net.icefighter.mobsbrawl.procedures;
 
+import net.minecraftforge.registries.ForgeRegistries;
+
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.ItemStack;
@@ -9,7 +11,10 @@ import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.BlockPos;
 
 public class FireboltRightclickedProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, ItemStack itemstack) {
@@ -30,6 +35,13 @@ public class FireboltRightclickedProcedure {
 				_entityToSpawn.setPos(x, (y + 1), z);
 				_entityToSpawn.shoot((entity.getLookAngle().x), (entity.getLookAngle().y), (entity.getLookAngle().z), 3, 0);
 				projectileLevel.addFreshEntity(_entityToSpawn);
+			}
+			if (world instanceof Level _level) {
+				if (!_level.isClientSide()) {
+					_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.dragon_fireball.explode")), SoundSource.PLAYERS, 1, 1);
+				} else {
+					_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.dragon_fireball.explode")), SoundSource.PLAYERS, 1, 1, false);
+				}
 			}
 			if (entity instanceof Player _player)
 				_player.getCooldowns().addCooldown(itemstack.getItem(), 60);
