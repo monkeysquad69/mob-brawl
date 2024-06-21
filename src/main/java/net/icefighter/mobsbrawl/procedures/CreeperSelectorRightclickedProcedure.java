c40656393e7de4eb41f4135b7f1c8cf9f84c8c4e
chunkyice <icefighter86@gmail.com>
2024-06-21 18:38:55 +0200

    some order on this shit -ice

--- src/main/java/net/icefighter/mobsbrawl/procedures/CreeperSelectorRightclickedProcedure.java
+++ src/main/java/net/icefighter/mobsbrawl/procedures/CreeperSelectorRightclickedProcedure.java
@@ -1,8 +1,27 @@
 package net.icefighter.mobsbrawl.procedures;
 
-import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.BlockPos;
 
 public class CreeperSelectorRightclickedProcedure {
-	public static void execute() {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, ItemStack itemstack) {
+		if (entity == null)
+			return;
+		entity.getPersistentData().putString("Class", "Creeper");
+		if (world instanceof Level _level) {
+			if (!_level.isClientSide()) {
+				_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.creeper.hurt")), SoundSource.PLAYERS, 1, 1);
+			} else {
+				_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.creeper.hurt")), SoundSource.PLAYERS, 1, 1, false);
+			}
+		}
+		itemstack.shrink(1);
 	}
 }
--- src/main/java/net/icefighter/mobsbrawl/procedures/TelluriumItemInInventoryTickProcedure.java
+++ src/main/java/net/icefighter/mobsbrawl/procedures/TelluriumItemInInventoryTickProcedure.java
@@ -1,6 +1,8 @@
 package net.icefighter.mobsbrawl.procedures;
 
-import net.minecraftforge.eventbus.api.Event;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
 
 public class TelluriumItemInInventoryTickProcedure {
 	public static void execute(Entity entity, ItemStack itemstack) {
